005f8dac5c0372efad42c18b764c92f0086b8d8f
akheel <akhlasdi23@gmail.com>
2017-09-20 23:32:49 +0530

    Added ng- bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbdButtonsCheckbox } from './buttons-checkbox';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    ReactiveFormsModule,
+    FormsModule,
+    BrowserModule,
+    NgbModule.forRoot()
+  ],
+  declarations: [
+    AppComponent,
+    NgbdButtonsCheckbox
   ],
   providers: [],
   bootstrap: [AppComponent]
